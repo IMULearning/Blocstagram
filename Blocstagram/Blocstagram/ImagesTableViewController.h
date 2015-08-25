9a59b4f5277c3970054b1bb5011e003580544c01
Weinan Qiu <davidiamyou@gmail.com>
2015-08-25 18:06:33 -0400

    Checkpoint 27

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Weinan Qiu on 2015-08-25.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
